c1ca8a33c8b38d9ff8b18ed9a83860c8d31fbb3d
mm-masahiro <hand5.mm9.mzh@gmail.com>
2021-12-16 20:36:41 +0900

    tutorial

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,14 @@
 <script lang="ts">
 	export let name: string;
+	export let text: string;
+	let hoge = 'hoge';
 </script>
 
 <main>
 	<h1>Hello {name}!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<h1>{text}</h1>
+	<p>{hoge}</p>
 </main>
 
 <style>
